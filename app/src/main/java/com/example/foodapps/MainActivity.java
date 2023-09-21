b652a77f31efe9396029f1190a220a89a5dbbfe1
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-09-22 00:39:10 +0700

    add another fragment and edit home fragment

--- app/src/main/java/com/example/foodapps/MainActivity.java
+++ app/src/main/java/com/example/foodapps/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.foodapps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Build;
 import android.os.Bundle;
+import com.example.foodapps.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.navBar.setOnItemSelectedListener(item -> {
+
+            if(item.getItemId() == R.id.homeId){
+            replaceFragment(new HomeFragment());
+
+            }else if (item.getItemId() == R.id.searchId){
+                replaceFragment(new SearchFragment());
+
+            }else if(item.getItemId() == R.id.cartId){
+                replaceFragment(new CartFragment());
+
+            } else if(item.getItemId() == R.id.profileId) {
+                replaceFragment(new ProfileFragment());
+
+            }
+
+            return true;
+        });
+
+
     }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrameLayout, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+    private void statusBarColor(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            getWindow().setStatusBarColor(getResources().getColor(R.color.greenTheme,this.getTheme()));
+        }else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setStatusBarColor(getResources().getColor(R.color.greenTheme));
+        }
+    }
+
 }
